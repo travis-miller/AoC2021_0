16b34485be087a4f8aca33d6a5a9e04c75fc87ad
travismiller <travismiller505@gmail.com>
2021-12-18 13:10:19 -0500

    Day 12 solutions

---
+++ day_12/part2.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to open input.txt file: %v", err)
+	}
+	defer f.Close()
+	caves := map[string]*cave{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		edge := strings.Split(scanner.Text(), "-")
+		if len(edge) != 2 {
+			log.Fatalf("Edge %s in unexpected format.", scanner.Text())
+		}
+		c, ok := caves[edge[0]]
+		if !ok {
+			isLarge := false
+			if edge[0] == strings.ToUpper(edge[0]) {
+				isLarge = true
+			}
+			c = &cave{edge[0], isLarge, []string{}}
+			caves[c.name] = c
+		}
+		c.edges = append(c.edges, edge[1])
+
+		c, ok = caves[edge[1]]
+		if !ok {
+			isLarge := false
+			if edge[1] == strings.ToUpper(edge[1]) {
+				isLarge = true
+			}
+			c = &cave{edge[1], isLarge, []string{}}
+			caves[c.name] = c
+		}
+		c.edges = append(c.edges, edge[0])
+	}
+	paths := findPaths("start", caves, []string{}, false)
+	fmt.Printf("There are %d paths.\n", len(paths))
+}
+
+type cave struct {
+	name    string
+	isLarge bool
+	edges   []string
+}
+
+func findPaths(curCave string, caves map[string]*cave, path []string, revisitedSmall bool) [][]string {
+	if curCave == "end" {
+		path = append(path, curCave)
+		return [][]string{path}
+	}
+	if !caves[curCave].isLarge {
+		for _, c := range path {
+			if curCave == c {
+				if curCave == "start" {
+					return [][]string{}
+				}
+				if revisitedSmall {
+					return [][]string{}
+				}
+				revisitedSmall = true
+			}
+		}
+	}
+	paths := [][]string{}
+	path = append(path, curCave)
+	for _, c := range caves[curCave].edges {
+		// Copy slice to prevent corruption
+		newPath := append([]string{}, path...)
+		paths = append(paths, findPaths(c, caves, newPath, revisitedSmall)...)
+	}
+	return paths
+}
